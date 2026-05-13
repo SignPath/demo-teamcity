b149b1c9fd4d3d3505e23f8a25e3ca7e9f196685
Paul Savoie <paul.savoie@signpath.io>
2026-05-13 13:53:37 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.buildSteps.signPathSubmitSigningRequest
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 version = "2024.12"
 
